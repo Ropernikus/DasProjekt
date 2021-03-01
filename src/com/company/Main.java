f5c84c938137be19603806caf396168683a595a6
Ropernikus <rmo@posteo.at>
2021-03-01 12:41:18 +0100

    new commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,59 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Beispiel 3 - Variablen
+        int zahl = 12;
+        System.out.println(zahl);
+
+
+        double ergebnis = 5/2;
+        int ergebnis2 = 5/2;
+
+        System.out.println(zahl++);
+        System.out.println(++zahl);
+        System.out.println(5/2);
+        System.out.println(ergebnis);
+        System.out.println(ergebnis2);
+
+
+
+        /* Beispiel 2 - Ausgabe
+	    System.out.println("     ___");
+	    System.out.println("    /  /\\");
+        System.out.println("   /  /::\\");
+        System.out.println("  /  /:/\\:\\");
+        System.out.println(" /  /:/~/::\\");
+        System.out.println("/__/:/ /:/\\:\\");
+        System.out.println("\\  \\:\\/:/__\\/");
+        System.out.println(" \\  \\::/");
+        System.out.println("  \\  \\:\\");
+        System.out.println("   \\  \\:\\");
+        System.out.println("    \\__\\/");
+
+
+        System.out.println("      ___");
+        System.out.println("     /  /\\");
+        System.out.println("    /  /::\\");
+        System.out.println("   /  /:/\\:\\");
+        System.out.println("  /  /:/  \\:\\");
+        System.out.println(" /  /:/   /:/");
+        System.out.println("/__/:/   /:/");
+        System.out.println("\\  \\:\\  /:/");
+        System.out.println(" \\  \\:\\/:/");
+        System.out.println("  \\  \\::/");
+        System.out.println("   \\  \\:\\");
+        System.out.println("    \\  \\:\\");
+        System.out.println("     \\  \\:\\");
+        System.out.println("      \\  \\:\\");
+        System.out.println("       \\__\\/");
+        */
+
+
+        // Beispiel 1
+        //System.out.println("Hello World!");
+
+    }
+}
